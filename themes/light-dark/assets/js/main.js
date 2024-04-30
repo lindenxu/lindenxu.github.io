fb005718d12706afdf8fee4c6ebdc7a1230b8e8f
Linden <19319855+lindenxu@users.noreply.github.com>
2024-04-30 13:03:03 +0800

    fix：Uncaught TypeError

--- themes/light-dark/assets/js/main.js
+++ themes/light-dark/assets/js/main.js
@@ -31,10 +31,10 @@
 
   document.addEventListener('click', function (event) {
     const target = event.target;
-    if (navbarMenuToggle.contains(target)) {
+    if (navbarMenuToggle && navbarMenuToggle.contains(target)) {
       navbarLang && navbarLang.classList.add('hidden');
       navbarMenu && navbarMenu.classList.toggle('hidden');
-    } else if (navbarLangToggle.contains(target)) {
+    } else if (navbarLangToggle && navbarLangToggle.contains(target)) {
       navbarMenu && navbarMenu.classList.add('hidden');
       navbarLang && navbarLang.classList.toggle('hidden');
     } else {
